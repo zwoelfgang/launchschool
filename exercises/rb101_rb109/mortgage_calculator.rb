b573918c108a60fab6c509b251396a35ee0d963c
Wolf Timm <wolfktimm@gmail.com>
2021-09-02 21:04:19 +0200

    Fixed some edge cases in mortgage

--- exercises/rb101_rb109/mortgage_calculator.rb
+++ exercises/rb101_rb109/mortgage_calculator.rb
@@ -1,4 +1,5 @@
 require 'yaml'
+require 'pry'
 
 PROMPTS = YAML.load_file('mortgage_prompts.yml')
 
@@ -7,49 +8,58 @@ def output(message)
 end
 
 def input
-  gets.chomp.to_f
+  gets.chomp
 end
 
-loop do
-  output(PROMPTS['welcome'])
-  output(PROMPTS['amount'])
-
-  loan_amount = input()
-
-  while loan_amount == 0.0
+def validation(value)
+  while value == 0
     output(PROMPTS['invalid'])
-    loan_amount = input()
+    value = input.to_f
   end
+  return value
+end
+
+output(PROMPTS['welcome'])
+
+loop do
+  output(PROMPTS['amount'])
+  loan_amount = input.to_f
+  validation(loan_amount)
 
   output(PROMPTS['apr'])
-
   apr = input
 
-  while apr == 0.0
-    output(PROMPTS['invalid'])
-    apr = input
+  if apr == "0"
+    output(PROMPTS['congrats'])
+    apr = apr.to_f
+  else
+    apr = apr.to_f
+    validation(apr)
   end
 
   output(PROMPTS['duration'])
-
-  loan_duration = input
-
-  while loan_duration == 0.0
-    output(PROMPTS['invalid'])
-    loan_duration = input
-  end
+  loan_duration = input.to_f
+  validation(loan_duration)
 
   monthly_interest = (apr / 100) / 12
   monthly_duration = loan_duration * 12
-  monthly_payment = loan_amount * (monthly_interest /
-                    (1 - (1 + monthly_interest)**(-monthly_duration)))
+
+  if apr != 0
+    monthly_payment = loan_amount * (monthly_interest /
+                      (1 - (1 + monthly_interest)**(-monthly_duration)))
+  else
+    monthly_payment = loan_amount / monthly_duration
+  end
+  
   dollar_payment = "$" + monthly_payment.round(2).to_s
 
   output(PROMPTS['outcome'])
   output(dollar_payment)
 
   output(PROMPTS['again'])
-  answer = gets.chomp
+  answer = input
 
   answer == 'y' ? TRUE : break
 end
+
+output(PROMPTS['goodbye'])
