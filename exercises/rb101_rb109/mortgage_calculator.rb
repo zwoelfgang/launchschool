bae640151618925767654c02fe254b2b7bcd0d5f
Wolf Timm <wolfktimm@gmail.com>
2021-09-03 12:19:55 +0200

    Fixed code after code review :)

--- exercises/rb101_rb109/mortgage_calculator.rb
+++ exercises/rb101_rb109/mortgage_calculator.rb
@@ -1,4 +1,6 @@
 require 'yaml'
+require 'io/console'
+require 'pry'
 
 PROMPTS = YAML.load_file('mortgage_prompts.yml')
 
@@ -8,6 +10,10 @@ end
 
 def input
   gets.chomp
+end
+
+def clear_screen
+  STDOUT.clear_screen
 end
 
 def validation(value)
@@ -26,50 +32,66 @@ def negative_check(value)
   value
 end
 
+clear_screen
+
 output(PROMPTS['welcome'])
 
-loop do
-  output(PROMPTS['amount'])
-  loan_amount = input.to_f
-  validation(loan_amount)
-  loan_amount = negative_check(loan_amount)
+catch :restart do
+  loop do
+    output(PROMPTS['amount'])
+    loan_amount = input.to_f
+    loan_amount = validation(loan_amount)
+    loan_amount = negative_check(loan_amount)
 
-  output(PROMPTS['apr'])
-  apr = input
+    output(PROMPTS['apr'])
+    apr = input
 
-  if apr == "0"
-    output(PROMPTS['congrats'])
-    apr = apr.to_f
-  else
-    apr = apr.to_f
-    validation(apr)
-    apr = negative_check(apr)
+    if apr == "0"
+      output(PROMPTS['congrats'])
+      apr = apr.to_f
+    else
+      apr = apr.to_f
+      apr = validation(apr)
+      apr = negative_check(apr)
+    end
+
+    output(PROMPTS['duration'])
+    loan_duration = input.to_f
+    loan_duration = validation(loan_duration)
+    loan_duration = negative_check(loan_duration)
+
+    monthly_interest = (apr / 100) / 12
+    monthly_duration = loan_duration * 12
+
+    if apr != 0
+      monthly_payment = loan_amount * (monthly_interest /
+                        (1 - (1 + monthly_interest)**(-monthly_duration)))
+    else
+      monthly_payment = loan_amount / monthly_duration
+    end
+
+    dollar_payment = "$" + monthly_payment.round(2).to_s
+
+    output(PROMPTS['outcome'])
+    output(dollar_payment)
+
+    output(PROMPTS['again'])
+    answer = input.downcase
+
+    loop do
+      case answer
+      when 'y'
+        clear_screen
+        break
+      when 'n'
+        throw :restart
+      else
+        output(PROMPTS['restart'])
+        answer = input.downcase
+      end
+    end
   end
-
-  output(PROMPTS['duration'])
-  loan_duration = input.to_f
-  validation(loan_duration)
-  loan_duration = negative_check(loan_duration)
-
-  monthly_interest = (apr / 100) / 12
-  monthly_duration = loan_duration * 12
-
-  if apr != 0
-    monthly_payment = loan_amount * (monthly_interest /
-                      (1 - (1 + monthly_interest)**(-monthly_duration)))
-  else
-    monthly_payment = loan_amount / monthly_duration
-  end
-
-  dollar_payment = "$" + monthly_payment.round(2).to_s
-
-  output(PROMPTS['outcome'])
-  output(dollar_payment)
-
-  output(PROMPTS['again'])
-  answer = input.downcase
-
-  answer == 'y' ? TRUE : break
 end
 
+clear_screen
 output(PROMPTS['goodbye'])
