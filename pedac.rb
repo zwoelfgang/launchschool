7615348997bec896abd00ba94632fdaae41ff202
Wolf Timm <wolfktimm@gmail.com>
2021-10-22 22:19:41 +0200

    Made a pedac template

---
+++ pedac.rb
@@ -0,0 +1,46 @@
+=begin
+________________________________________________________________________________
+
+*****Problem*****
+________________________________________________________________________________
+Input:
+
+  # What is the form of the data you want to transform?
+
+Output:
+
+  # What is the form of the data you want the program to either store or print?
+
+Explicit reqs:
+
+  # What does the problem say about what you should do?
+
+Implicit reqs:
+
+  # What does the problem NOT say about what you need to have covered?
+________________________________________________________________________________
+
+*****Examples/Test cases*****
+________________________________________________________________________________
+Test cases:
+
+  # What are some examples of expected outputs for a given input? Be thorough
+________________________________________________________________________________
+
+*****Data structures*****
+________________________________________________________________________________
+Data manipulation strategy:
+
+  # What forms do your data take on while the program is running?
+________________________________________________________________________________
+
+*****Algorithm*****
+________________________________________________________________________________
+Step-by-step process (plaintext or pseudocode):
+
+  # The logical process by which you intend to tackle the problem at hand
+________________________________________________________________________________
+
+Time to code!
+
+=end
